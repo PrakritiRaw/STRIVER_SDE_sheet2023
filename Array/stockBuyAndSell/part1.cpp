61b93af3633a2ec8a8273b744c31e20d47504498
Prakriti Rawat <rawprakriti@gmail.com>
2023-07-02 20:47:59 +0530

    Create part1.cpp

---
+++ Array/stockBuyAndSell/part1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit=0;
+        
+        for(int i=1;i<prices.size();i++)
+        {
+            profit=max(prices[i]-mini,profit);
+            mini=min(mini,prices[i]);
+        }
+        return profit;
+    }
+};
